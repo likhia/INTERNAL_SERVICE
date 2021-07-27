b64f0884243ee8dee6648a919578695ff975efbe
opentlc-mgr <lilim@redhat.com>
2021-07-27 12:13:06 +0000

    updates

--- src/main/java/com/workshop/main/service/ServiceEndPoint.java
+++ src/main/java/com/workshop/main/service/ServiceEndPoint.java
@@ -6,9 +6,17 @@ import javax.ws.rs.Produces;
 
 import org.springframework.stereotype.Component;
 
+//import classes (1)
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
 @Path("/service")
 @Component
 public class ServiceEndPoint {
+
+    //Get URI from properties
+    @Value("${common.uri}")
+    private String commonURI; 
 
     @POST
     @Path("/registervehicle")
@@ -23,6 +31,26 @@ public class ServiceEndPoint {
         System.out.println("Created Year : " + carInfo.getCreatedYear());
         System.out.println("Used: " + carInfo.getUsed());
 
+        //Invoke common webservice to get the car plate number based on Type attribute
+        plateNo = getCarPlate(carInfo.getType());
+
         return plateNo;
+    }
+    
+    /**
+     * Invoke common service to get the car plate no. 
+     * @param type
+     * @return car plate No
+     */
+    private String getCarPlate(String type) {
+
+        //String uri = "http://services-common.apps.cluster-2e68.2e68.sandbox1783.opentlc.com/api/common/carplate?type=" + type;
+        String uri = "http://" + commonURI + "/api/common/carplate?type=" + type;
+
+        System.out.println("URL: " + uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+        
+        return (String) restTemplate.getForObject(uri, String.class);
     }
 }
