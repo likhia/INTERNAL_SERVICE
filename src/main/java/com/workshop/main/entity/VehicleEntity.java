ef9dade88c953257cac0c306b95fef06daae4b29
opentlc-mgr <lilim@redhat.com>
2021-07-29 14:32:31 +0000

    updates

--- src/main/java/com/workshop/main/entity/VehicleEntity.java
+++ src/main/java/com/workshop/main/entity/VehicleEntity.java
@@ -3,14 +3,18 @@ package com.workshop.main.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name = "VEHICLES")
 public class VehicleEntity {
 
-    @Id 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String carplate = null;
-    
     private String model = null;
     private String color = null;
     private String engineCapacity = null;
